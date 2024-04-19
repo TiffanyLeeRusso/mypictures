f697b771fa9eb9b782a7f4c2c9823065b9ee818d
TiffanyLeeRusso <tiffanyleerusso@gmail.com>
2024-04-18 17:59:09 -0700

    Switch logo to loremflickr

--- src/jsx/Header.js
+++ src/jsx/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className="navbar navbar-inverse navbar-static-top">
-        <img src="https://placekitten.com/70/70" alt="logo" />
+        <img src="https://loremflickr.com/80/80/cat" alt="logo" />
         <h2>Welcome to MyPictures, a React experiment</h2>
       </div>
     );
